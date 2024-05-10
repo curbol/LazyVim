b2a6ad5a8d386659da88e0c68a1d31ae6ae79b31
curbol <curtis.bollinger@gmail.com>
2024-05-09 23:25:53 -0700

    feat: add gitlinker

---
+++ lua/plugins/gitlinker.lua
@@ -0,0 +1,10 @@
+return {
+  "ruifm/gitlinker.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {},
+  keys = {
+    { "<leader>gy", desc = "Copy GitHub URL", mode = { "n", "v" } },
+  },
+}
