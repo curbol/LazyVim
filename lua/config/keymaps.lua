ad48562b3ab6970708a7a2e2782b0caf5debda05
curbol <curtis.bollinger@gmail.com>
2024-05-09 23:25:39 -0700

    feat: add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in place while joining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Center cursor when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Center cursor when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+-- Center cursor when navigating the quickfix list
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- Center cursor when navigating the location list
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste and keep buffer" })
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["xy]], { desc = "Yank into the 'x' register" })
+vim.keymap.set("n", "<leader>Y", [["xY]], { desc = "Yank into the 'x' register" })
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to void register" })
