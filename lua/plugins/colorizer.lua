10dd17ca98a457794174a64f7516a770d5e71ae1
curbol <curtis.bollinger@gmail.com>
2024-05-15 16:47:55 -0700

    feat: configure colorscheme/colorizer

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,9 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  event = "LazyFile",
+  opts = {
+    user_default_options = {
+      mode = "virtualtext",
+    },
+  },
+}
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+  {
     "sainnhe/gruvbox-material",
     lazy = true,
     name = "gruvbox-material",
@@ -13,7 +19,7 @@ return {
       vim.g.gruvbox_material_transparent_background = 1
       -- vim.g.gruvbox_material_dim_inactive_windows = 0
       -- vim.g.gruvbox_material_visual = "grey background"
-      vim.g.gruvbox_material_menu_selection_background = "orange"
+      vim.g.gruvbox_material_menu_selection_background = "blue"
       -- vim.g.gruvbox_material_sign_column_background = "none"
       -- vim.g.gruvbox_material_spell_foreground = "none"
       -- vim.g.gruvbox_material_ui_contrast = "low"
@@ -22,7 +28,7 @@ return {
       -- vim.g.gruvbox_material_diagnostic_text_highlight = 0
       -- vim.g.gruvbox_material_diagnostic_line_highlight = 0
       vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-      vim.g.gruvbox_material_current_word = "grey background" -- 'grey background', 'bold', 'underline', 'italic'
+      vim.g.gruvbox_material_current_word = "underline" -- 'grey background', 'bold', 'underline', 'italic'
       -- vim.g.gruvbox_material_disable_terminal_colors = 0
       -- vim.g.gruvbox_material_statusline_style = "default"
       -- vim.g.gruvbox_material_lightline_disable_bold = 0
