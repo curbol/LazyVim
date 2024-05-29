d3fea8b020a3efaab21109513e1b05f3d6b81093
curbol <curtis.bollinger@gmail.com>
2024-05-29 16:57:23 -0700

    feat: plugin updates
    - remove maximizer because maximizing is built in to LazyVim now
    - update gopls with build tags so that e2e tests get diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,9 @@ return {
     -- Disable warnings like "struct with 168 pointer bytes could be 152"
     opts.servers.gopls.settings.gopls.analyses.fieldalignment = false
 
+    -- Add build flags for gopls to include integration and e2e tests so that diagnostics are shown
+    opts.servers.gopls.settings.gopls.buildFlags = { "-tags=integration,e2e" }
+
     -- Disable inlay hints (e.g. type hints)
     opts.inlay_hints.enabled = false
   end,
