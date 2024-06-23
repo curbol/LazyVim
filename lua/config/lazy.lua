e5b67673bfbe5a0d40a4775b349f040d19ae60a9
curbol <curtis.bollinger@gmail.com>
2024-06-23 15:31:12 -0700

    chore: update plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.ruby" },
+    -- { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
