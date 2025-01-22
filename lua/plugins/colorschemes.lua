eb810f520c00fbf75fb973760c71fecc32644b48
curbol <curtis.bollinger@gmail.com>
2025-01-21 16:40:21 -0800

    remove hacking colorscheme stuff

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -40,20 +40,20 @@ return {
       }
 
       -- Set custom highlights
-      vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
-        group = vim.api.nvim_create_augroup("Color", {}),
-        pattern = "*",
-        callback = function()
-          -- override color of notification messages in bottom right corner
-          -- NOTE: #3a3a3a ends up being #282828 for some reason and NONE doesn't work 󰽌
-          vim.api.nvim_set_hl(0, "MsgArea", { bg = "#3a3a3a" })
-
-          -- override color of window bar enabled by `vim.opt.winbar`
-          vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE", fg = "#7c6f64" })
-          vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE", fg = "#a89984" })
-        end,
-        desc = "Set custom highlights",
-      })
+      -- vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+      --   group = vim.api.nvim_create_augroup("Color", {}),
+      --   pattern = "*",
+      --   callback = function()
+      --     -- override color of notification messages in bottom right corner
+      --     -- NOTE: #3a3a3a ends up being #282828 for some reason and NONE doesn't work 󰽌
+      --     vim.api.nvim_set_hl(0, "MsgArea", { bg = "#3a3a3a" })
+      --
+      --     -- override color of window bar enabled by `vim.opt.winbar`
+      --     vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE", fg = "#7c6f64" })
+      --     vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE", fg = "#a89984" })
+      --   end,
+      --   desc = "Set custom highlights",
+      -- })
     end,
   },
 }
