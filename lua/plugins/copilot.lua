077e18cb4c179af79bb9cd0fec7d3cbb677017b5
curbol <curtis.bollinger@gmail.com>
2025-04-07 13:09:27 -0700

    temp copilot fix

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+-- Temp fix for vim.schedule error
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    optional = true,
+    opts = function()
+      require("copilot.api").status = require("copilot.status")
+    end,
+  },
+}
