e8f8f8b7ed093e23613e8037148f20c81c373a41
curbol <curtis.bollinger@gmail.com>
2026-02-27 14:21:02 -0800

    chore(nvim): remove smart-splits plugin

--- lua/plugins/smart-splits.lua
+++
@@ -1,8 +0,0 @@
--- No smart-splits plugin: using plain vim keymaps for window nav/move.
--- ctrl+arrows = navigate windows, ctrl+shift+arrows = move (swap) windows.
-return {
-  {
-    "mrjones2014/smart-splits.nvim",
-    enabled = false,
-  },
-}
