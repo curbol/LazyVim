a5fc73af1a7df1e352001f8b0f7934cbb9debe4c
curbol <curtis.bollinger@gmail.com>
2026-02-27 17:15:43 -0800

    improve splits config

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,10 +1,13 @@
 return {
   "mrjones2014/smart-splits.nvim",
   event = "VeryLazy",
+  opts = {
+    cursor_follows_swapped_bufs = true,
+  },
   keys = {
-    { "<C-S-Left>",  function() require("smart-splits").swap_buf_left() end,  desc = "Swap window left" },
-    { "<C-S-Right>", function() require("smart-splits").swap_buf_right() end, desc = "Swap window right" },
-    { "<C-S-Up>",    function() require("smart-splits").swap_buf_up() end,    desc = "Swap window up" },
-    { "<C-S-Down>",  function() require("smart-splits").swap_buf_down() end,  desc = "Swap window down" },
+    { "<C-S-Left>",  function() require("smart-splits").swap_buf_left() end,  desc = "Move window left" },
+    { "<C-S-Right>", function() require("smart-splits").swap_buf_right() end, desc = "Move window right" },
+    { "<C-S-Up>",    function() require("smart-splits").swap_buf_up() end,    desc = "Move window up" },
+    { "<C-S-Down>",  function() require("smart-splits").swap_buf_down() end,  desc = "Move window down" },
   },
 }
